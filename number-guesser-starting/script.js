f2d696ac3fd02ada94105f124c26126b9229edee
stephen <rstephen2000@gmail.com>
2023-08-20 23:20:37 +0530

    Number Guesser completed

--- number-guesser-starting/script.js
+++ number-guesser-starting/script.js
@@ -4,3 +4,12 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+const generateTarget = () => Math.floor(Math.random() * 10);
+
+const compareGuesses = (humanGuess, computerGuess, secretTarget) =>
+  Math.abs(secretTarget - humanGuess) <= Math.abs(secretTarget - computerGuess);
+
+const updateScore = (winner) =>
+  winner === "computer" ? computerScore++ : humanScore++;
+
+const advanceRound = () => currentRoundNumber++;
